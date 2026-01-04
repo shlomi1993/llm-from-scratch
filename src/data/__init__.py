1ca83f666371e6e8b15b797483cfdebfc0d737bd
Shlomi Ben Shushan <shlomiggl@gmail.com>
2026-01-04 19:25:27 +0200

    WIP

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -4,12 +4,16 @@ Data module
 This module contains data-related classes and functions, including datasets and dataloaders for training and evaluation.
 """
 
-from .datasets.pretrain import GptDatasetV1
+from .datasets.instruction import InstructionDataset
 from .datasets.classification import SpamDataset
+from .datasets.pretrain import GptDatasetV1
+from .formatting import format_input
 from .loaders import GptDataloaderV1
 
 __all__ = [
     "GptDatasetV1",
     "SpamDataset",
+    "InstructionDataset",
     "GptDataloaderV1",
+    "format_input",
 ]
